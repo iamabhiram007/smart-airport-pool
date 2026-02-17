55eb1a29699d4b676d62616d6715207475b9b6c9
abhiram <abhiram2345abhiram@gmail.com>
2026-02-17 23:33:55 +0530

    Read DB URL from environment variable

--- app/db/database.py
+++ app/db/database.py
@@ -2,9 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# Read from environment variable
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/ride_pool"
+    "postgresql://localhost:5432/ride_pool"  # fallback default
 )
 
 engine = create_engine(DATABASE_URL)
@@ -12,7 +13,6 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 from sqlalchemy.orm import Session
 
