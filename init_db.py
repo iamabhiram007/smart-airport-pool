1dc369a531a34f0b252e019c50526c7a62551412
abhiram <abhiram2345abhiram@gmail.com>
2026-02-17 23:37:03 +0530

    Ensure clean DB schema initialization

--- init_db.py
+++ init_db.py
@@ -1,8 +1,10 @@
 from app.db.database import engine, Base
-from app.models.ride_group import RideGroup
+from app.models import ride_group  # ensure model imported
+
+print("Dropping old tables...")
+Base.metadata.drop_all(bind=engine)
 
 print("Creating database tables...")
-
 Base.metadata.create_all(bind=engine)
 
 print("Tables created successfully!")
